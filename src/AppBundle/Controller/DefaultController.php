6b010aed54fc55c929d427a0573800119c0f2961
Miro Svrtan <miro@mirosvrtan.me>
2016-03-05 11:29:04 +0100

    [starter-edition] Cleaning up AppBundle default controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController.
+ */
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @SuppressWarnings("PHPMD.UnusedFormalParameter")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
