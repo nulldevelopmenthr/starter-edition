fa3f17eedd02a378f717e2c245b1193639d9cf28
Miro Svrtan <miro@mirosvrtan.me>
2016-05-07 12:20:34 +0200

    [starter-edition] Upgraded CircleCI to use Ubuntu Trusty and run PHP 7 (nulldevelopmenthr/starter-edition#2)

---
+++ etc/circleci/setup-nginx.sh
@@ -0,0 +1,7 @@
+sudo apt-get install nginx
+sudo mkdir /etc/nginx/ssl/
+sudo cp etc/provisioning/roles/nginx/files/ssl/server.crt /etc/nginx/ssl/server.crt
+sudo cp etc/provisioning/roles/nginx/files/ssl/server.key /etc/nginx/ssl/server.key
+sed -i 's/PROJECT_NAME/'"$CIRCLE_PROJECT_REPONAME"'/' etc/circleci/symfony2-test-application.conf
+sudo cp etc/circleci/symfony2-test-application.conf /etc/nginx/sites-enabled/
+sudo service nginx restart
