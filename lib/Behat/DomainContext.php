8f8521ba5b0e4d96c80294c773faf86a89f6dd46
Miro Svrtan <miro@mirosvrtan.me>
2017-06-10 21:28:22 +0200

    [starter edition] Make all file classes strict

--- lib/Behat/DomainContext.php
+++ lib/Behat/DomainContext.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Resources\Behat;
 
 use Behat\Gherkin\Node\TableNode;
--- lib/Behat/ScreenshotTrait.php
+++ lib/Behat/ScreenshotTrait.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Resources\Behat;
 
 use Behat\Behat\Hook\Scope\AfterStepScope;
